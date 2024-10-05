f0a0496a3a1b2669c41dabeccbf3222cd73bf9ac
Flávia Prouvot <flavia.prouvot@gmail.com>
2024-10-04 22:19:27 -0300

    temp.py

---
+++ temp.py
@@ -0,0 +1,12 @@
+### A lista de temperaturas de Mons, na Bélgica, foi armazenada na lista T = [-10, -8, 0, 1, 2, 5, -2, -4]. Faça um programa que imprima a menor e a maior temperatura, assim como a temperatura média.
+
+x=0
+T =[-10, -8, 0, 1, 2, 5, -2, -4]
+maior= max(T)
+menor= min(T)
+soma=((-10) + (-8) + (0) + (1) + (2) + (5) + (-2) + (-4))
+media=soma/8
+
+print (maior)
+print (menor)
+print (media)
